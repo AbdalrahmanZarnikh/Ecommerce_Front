07f2df36b13898c9cf33dd00aaba0c46dd6c121f
AbdAlrahman13 <blalabo89@gmail.com>
2025-06-06 16:59:12 +0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import "./App.css"
 function App() {
 
   return (
-    <div className="  border rounded-3xl flex justify-center items-center h-screen  ">
+    <div className=" flex justify-center items-center h-screen  ">
       <h1 className="text-red-700 text-6xl bg-black rounded-4xl p-10 hover:bg-gray-800 hover:cursor-pointer">  Ecommerce App </h1>
     </div>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
 import Router from './Router/Router.jsx'
 
 createRoot(document.getElementById('root')).render(
