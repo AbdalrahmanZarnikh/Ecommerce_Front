038cdf4be1c75194f0fbf3aea058aea971d23171
AbdAlrahman13 <blalabo89@gmail.com>
2025-07-01 18:57:17 +0300

    add lottie files for loading and notfound status

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import  { useEffect, useState } from "react";
 import { BiSearch } from "react-icons/bi";
 import Container from "../components/Container/Container";
 import { useDispatch, useSelector } from "react-redux";
