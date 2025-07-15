fdac08d9922957294d7aac69af8aedad413d42ef
AbdAlrahman13 <blalabo89@gmail.com>
2025-07-15 19:13:58 +0300

    end  orders page in dashboard user

--- src/components/CategoryCard/CategoryCard.jsx
+++ src/components/CategoryCard/CategoryCard.jsx
@@ -7,7 +7,7 @@ const CategoryCard = memo(({ image, name, id, to }) => {
 
   return (
     <div
-      className="cursor-pointer w-64 h-64 relative rounded-2xl overflow-hidden hover:scale-105 hover:shadow-xl transition-all duration-300 ease-in-out mt-6"
+      className={`cursor-pointer ${to=="categories"?"w-64 h-64":"w-40 h-40"} relative rounded-2xl overflow-hidden hover:scale-105 hover:shadow-xl transition-all duration-300 ease-in-out mt-6 bg-gray-200`}
       onClick={() => {
         navigate(`/${to}/${id}`);
       }}
@@ -17,9 +17,9 @@ const CategoryCard = memo(({ image, name, id, to }) => {
         className="absolute inset-0 w-full h-full object-cover brightness-50 transition-all duration-300"
         alt={name}
       />
-      <div className="absolute inset-0 flex items-center justify-center">
+     {to=="categories" &&     <div className="absolute inset-0 flex items-center justify-center">
         <span className="text-white font-semibold text-xl text-center px-2">{name}</span>
-      </div>
+      </div>}
     </div>
   );
 });
